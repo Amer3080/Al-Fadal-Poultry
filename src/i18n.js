f25cb317ab65ceafc7e3319cae9a1956a1cbdc35
Amer3080 <mohamed.amer3080@gmail.com>
2025-09-03 06:33:39 +0300

    Last Update

--- src/i18n.js
+++ src/i18n.js
@@ -3,6 +3,7 @@ import { initReactI18next } from "react-i18next";
 
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+import translationEN from "../public/locales/en/translation.json";
 // don't want to use this?
 // have a look at the Quick start guide
 // for passing in lng and translations on init
@@ -20,6 +21,9 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    resources: {
+      en: { translation: translationEN },
+    },
     lng: "en",
     fallbackLng: "en",
     debug: true,
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import "./i18n";
+// eslint-disable-next-line no-unused-vars
+import i18n from "./i18n";
 import DataContextProvider from "./Components/Context/DataContext.jsx";
 
 // MUI theme imports
