9048d892391bdf9717dcda0a167a58b653d10fe1
Amer3080 <mohamed.amer3080@gmail.com>
2025-09-13 07:42:13 +0300

    Last Edit td

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,8 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import purgecss from "vite-plugin-purgecss";
 import { visualizer } from "rollup-plugin-visualizer";
-import { imagetools } from "vite-imagetools";
-
+import purgecss from "vite-plugin-purgecss";
 export default defineConfig({
-  base: "/",
-  assetsInclude: ["**/*.avif"],
-  plugins: [
-    imagetools({
-      include: ["**/*.avif", "**/*.png", "**/*.jpg", "**/*.webp"],
-    }),
-    react(),
-    visualizer({
-      open: true,
-      filename: "stats.html",
-      gzipSize: true,
-      brotliSize: true,
-    }),
-
-    // purgecss({
-    //   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-    //   defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    //   safelist: [
-    //     /^Mui/,
-    //     "active",
-    //     /^swiper/,
-    //     /^slide/,
-    //     /^slide-image/,
-    //     /^text_content/,
-    //   ],
-    // }),
-  ],
-
-  resolve: {
-    alias: { "@mui/styled-engine": "@mui/styled-engine-sc" },
-    dedupe: ["react", "react-dom", "styled-components"],
-  },
-
-  optimizeDeps: {
-    include: ["styled-components", "@mui/styled-engine-sc"],
-  },
-
   build: {
     sourcemap: true,
     rollupOptions: {
@@ -53,5 +13,26 @@ export default defineConfig({
         },
       },
     },
+  },
+  plugins: [
+    purgecss({
+      content: ["./index.html", "./src/**/*.jsx"], // Adjust paths to match your project
+      safelist: [/^Mui/, /^slick/, "active"], // Keep important dynamic classes
+    }),
+    react(),
+    visualizer({
+      open: true, // open report in browser
+      filename: "stats.html", // report filename
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
+  resolve: {
+    alias: { "@mui/styled-engine": "@mui/styled-engine-sc" },
+    dedupe: ["react", "react-dom", "styled-components"],
+  },
+  optimizeDeps: {
+    // pre-bundle styled-components & the MUI SC engine
+    include: ["styled-components", "@mui/styled-engine-sc"],
   },
 });
